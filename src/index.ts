8f6e80f23b09f2dd02c78f0ecc3a2e6f19230000
DCloud_LXH <283700113@qq.com>
2025-10-30 11:43:13 +0800

    feat: 使用 markdown-it 配置中的 langPrefix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import MarkdownIt from "markdown-it"
 import Renderer from "markdown-it/lib/renderer"
 import Token from "markdown-it/lib/token"
 import { Context, Plugin } from '@vuepress/types';
-import path from "path";
 
 const escapeHtml = (str: string) => String(str)
   .replace(/&/g, '&amp;')
@@ -11,21 +10,21 @@ const escapeHtml = (str: string) => String(str)
   .replace(/"/g, '&quot;')
   .replace(/'/g, '&#39;');
 
-export const noscriptCodePlugin = (md: MarkdownIt, options: Options) => {
+export const noscriptCodePlugin = (md: MarkdownIt, pluginOptions: Options) => {
   const defaultRender = md.renderer.rules.fence || function (tokens: Token[], idx: number, options: any, env: any, renderer: Renderer) {
     return renderer.renderToken(tokens, idx, options);
   };
 
-  md.renderer.rules.fence = (tokens: Token[], idx: number, options: any, env: any, renderer: Renderer) => {
+  md.renderer.rules.fence = (tokens: Token[], idx: number, options: MarkdownIt.Options, env: any, renderer: Renderer) => {
     const token = tokens[idx];
     const code = token.content.trim();
     const lang = token.info.trim();
 
     const defaultRenderedCode = defaultRender(tokens, idx, options, env, renderer);
 
-    const noscriptContent = `<pre v-pre><code class="language-${escapeHtml(lang)}">${escapeHtml(code)}</code></pre>`;
+    const noscriptContent = `<pre v-pre><code class="${options.langPrefix}${escapeHtml(lang)}">${escapeHtml(code)}</code></pre>`;
     const noscriptTag = `<div class="ai noscript" style="display: none;">
-  <p>${options.AIPrompt || '示例源码如下，请查看 pre > code 标签中的内容'}</p>
+  <p>${pluginOptions.AIPrompt || '示例源码如下，请查看 pre > code 标签中的内容'}</p>
   ${noscriptContent}
 </div>`;
 
